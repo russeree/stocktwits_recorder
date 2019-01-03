8ffc420d8f02f3cedab6a64d36227cbda2354941
russeree <reese.russell@ymail.com>
2019-01-03 01:30:38 -0800

    Will Obtain all Bear & Bull sentiment indicators from a given symbol.

--- stream_read.py
+++ stream_read.py
@@ -7,6 +7,7 @@ import time
 from multiprocessing import Pool
 # Selenium Requirements
 from selenium import webdriver
+from selenium.common.exceptions import *
 from selenium.webdriver.common.keys import Keys
 ##
 class StockTwitsStreamRecorder:
@@ -17,6 +18,7 @@ class StockTwitsStreamRecorder:
         self.stockTwitsURLForm = "https://www.stocktwits.com/symbol/{}"
         self.stockTwitsStreamButton = 'StreamPlayButton__icon'
         self.stockTwitsMessageId = 'MessageList__item'
+        self.stockTwitsSentimentIncicatorPartialId = 'MessageStreamView__sentiment'
         print("Stocktwits Stream Recorder Activated")
     ##
     # @desc: When the Class is destroyed give the user a freindly reminder that it has closed successfuly
@@ -25,7 +27,7 @@ class StockTwitsStreamRecorder:
     ##
     # @desc: This is the stream recording loop: WARN: MANY MAGIC NUMBERS
     # @param:[symbol] string for the symbol that will be grabbed from the stocktwits website
-    def visitSite(self,symbol):
+    def twitStreamRecorder(self,symbol):
         print("Stocktwits Stream of ${} has began recording.".format(symbol))
         driver = webdriver.Chrome('./chromedriver')
         try: #Visit the StockTwits Ticker Symbol
@@ -40,12 +42,30 @@ class StockTwitsStreamRecorder:
             print("${} Failed to begin streaming user 'Twits' with Exception:".format(symbol))
             print('\t{}'.format(str(E)))
             return "${} Recoder Sream Failure".format(symbol)
-        while True:
-            time.sleep(5)
-            print(str(len(driver.find_elements_by_xpath('//*[contains(@class, "' + self.stockTwitsMessageId +'")]')))) #This is the equivilant of pointing to each message that comes though the stream
+        while True: #The Continuious Monitoring Loop
+            try:
+                _onScreenTwits = driver.find_elements_by_xpath('//*[contains(@class, "' + self.stockTwitsMessageId +'")]') #This is the equivilant of pointing to each message that comes though the stream
+                for twit in _onScreenTwits:
+                    try:
+                        _x = twit.find_element_by_xpath('.//*[contains(@class, "' + self.stockTwitsSentimentIncicatorPartialId +'")]').text
+                        if str(_x) == '':
+                            _x = 'None'
+                    except NoSuchElementException:
+                        _x = 'None'
+                    finally:
+                        print(_x)
+                print("----UPDATE----")
+                time.sleep(5)
+            except Exception as E:
+                print("${} Failed to read 'twit' with Exception:".format(symbol))
+                print('\t{}'.format(str(E)))
+                return "${} Recoder Sream Failure".format(symbol)
+        driver.close()
         return "${} Recoder Success".format(symbol)
+
 #Example Process Pool of Recorders
 if __name__ == '__main__':
     x = StockTwitsStreamRecorder()
     with Pool(5) as p:
-        print(p.map(x.visitSite,['SPY']))
+        print(p.map(x.twitStreamRecorder,['SPY']))
+    sys.exit(0)
